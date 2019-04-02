0bde7d1cc4b6648934ddf18d9cbb78f41d1b4d6b
DASHA <dariaepaneshnikova@icloud.com>
2019-04-02 21:04:40 +0200

    priceRisk works fine, made a mistake, fixed it, saving console.logs in commit for futue copypasting

--- client/src/components/tickets/TicketDetails.js
+++ client/src/components/tickets/TicketDetails.js
@@ -82,22 +82,25 @@ priceRisk = () => {
   render() {
     const {ticket} = this.props 
 
-    if (this.props) {
+    if (this.props.ticket && this.props.tickets) {
  
    const priceRisk = () => {
       const allTickets = this.props.tickets.length
-      console.log(this.props)
+      console.log(allTickets)
       const manyTicketsPrice = this.props.tickets.map(ticket => ticket.price)
+      console.log(manyTicketsPrice)
       const total = manyTicketsPrice.reduce((acc, value) => acc + value, 0)
+      console.log(total)
       const average = total / allTickets
+      console.log(average)
       // 	* if a ticket is X% cheaper than the average price, add X% to the risk 
     // 	* if a ticket is X% more expensive than the average price, 
     //deduct X% from the risk, with a maximum of 10% deduction
-      const risk = (-100 * 400 / average - 100)
+      const risk = (100 * this.props.ticket.price / average - 100)
       if (risk < -10) {
-        return -10
+        return console.log(risk-10)
       } else {
-        return (console.log('risk'))
+        return (console.log(risk))
       }
     }
 
