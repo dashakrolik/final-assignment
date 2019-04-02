baec27d0e65645289443fb7695417a19638f99cb
DASHA <dariaepaneshnikova@icloud.com>
2019-04-02 18:45:13 +0200

    add null condition to form

--- client/src/components/tickets/TicketForm.js
+++ client/src/components/tickets/TicketForm.js
@@ -22,18 +22,18 @@ export default class TicketForm extends PureComponent {
 			<form onSubmit={this.handleSubmit}>
 				<label>Url</label>
 					<input name="url" value={
-						this.state.url !== undefined ? this.state.picture : values.picture || null
+						this.state.url !== null || undefined ? this.state.picture : values.picture || null
 					} onChange={ this.handleChange } />
 
         <label>Price</label>
 					<input name="price" value={
-						this.state.price !== undefined ? this.state.price : values.price || null
+						this.state.price !== null || undefined ? this.state.price : values.price || null
 					} onChange={ this.handleChange } />
 
 
 				<label >Description</label>
 					<input name="description" value={
-						this.state.description !== undefined ? this.state.description : values.description || null
+						this.state.description !== null || undefined ? this.state.description : values.description || null
 					} onChange={ this.handleChange } />
 
 				<button type="submit">Submit</button>
