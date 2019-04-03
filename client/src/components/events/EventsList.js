b0f394bc2f866b818dcea7f66503db30bf0de2f3
DASHA <dariaepaneshnikova@icloud.com>
2019-04-03 21:20:37 +0200

    only current events are rendered on the page

--- client/src/components/events/EventsList.js
+++ client/src/components/events/EventsList.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import EventForm from './EventForm';
 import { getUsers } from '../../actions/users';
 
+
 class EventsList extends PureComponent{
   createEvent = (event) => {
     this.props.createEvent(event)
@@ -13,52 +14,28 @@ class EventsList extends PureComponent{
     this.props.loadEvents()
     this.props.getUsers()
   }
+  state = {}
+
+  returnCurrentEvents = () => {
+    let MyDate = new Date();
+    let CurrenDateString;
+    MyDate.setDate(MyDate.getDate());
+    CurrenDateString = MyDate.getFullYear() + '-' + ('0' + (MyDate.getMonth()+1)).slice(-2) + '-' + ('0' + MyDate.getDate()).slice(-2);
+    const currentEvents =  this.props.events.filter(event => event.end !== CurrenDateString)
+    return currentEvents
+  }
   
   render() {
     const {events} = this.props 
-
-    //Compare the array of dates to currentdate
-    const dateComparison = () => {
-      let array = this.props.events.map(event => event.end)
-      console.log(array)
-      let MyDate = new Date();
-      let CurrenDateString;
-      MyDate.setDate(MyDate.getDate());
-      CurrenDateString = MyDate.getFullYear() + '-' + ('0' + (MyDate.getMonth()+1)).slice(-2) + '-' + ('0' + MyDate.getDate()).slice(-2);
-      console.log(CurrenDateString)
-      let value1 = CurrenDateString;  //compare to current date
-      let value2 = "2019-04-05";  //is in array
-
-      function isInArray(array, value) {
-      return (array.find(item => {return item === value}) || []).length > 0;
-}
-
-//if the current date is in the array of end dates for events then that would mean the
-//event finishes today, which means the event is not current anymore => do not show the event if currentdate
-// is inside of the array
-// so if isInArray returns true do not show that event
-// if returns false show the event
-
-console.log(isInArray(array, value1));
-console.log(isInArray(array, value2));
-    }
+    console.log(this.props)
+    this.state = this.returnCurrentEvents()
+    console.log(this.state)
     
-    const returnCurrentEvents = () => {
-      let MyDate = new Date();
-      let CurrenDateString;
-      MyDate.setDate(MyDate.getDate());
-      CurrenDateString = MyDate.getFullYear() + '-' + ('0' + (MyDate.getMonth()+1)).slice(-2) + '-' + ('0' + MyDate.getDate()).slice(-2);
-      if (!dateComparison()) {
-        const currentEvents =  this.props.events.filter(event => event.end !== CurrenDateString)
-        return console.log(currentEvents)
-      }
-    }
-    returnCurrentEvents()
     return (
     <div>
       <br></br><br></br><br></br><br></br><br></br>
         <h1>Events</h1>
-          { events.map(event => 
+          { this.state.map(event => 
             (<div key={event.id}>
             <img src={event.url}/>
             <p><Link to={`/events/${event.id}`}>{event.name}</Link></p>
