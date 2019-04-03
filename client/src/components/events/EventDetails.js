b31c2feb936efe6b6e31b4619242833b198e7764
DASHA <dariaepaneshnikova@icloud.com>
2019-04-03 18:56:27 +0200

    dont know how to pass risk from ticketdetails to parent, so copied risk to eventdetails and trying to adjust it to calculate risk there as well

--- client/src/components/events/EventDetails.js
+++ client/src/components/events/EventDetails.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 import { loadEvent } from '../../actions/events';
 import { getUsers } from '../../actions/users';
-import { loadSelectedTickets, createTicket } from '../../actions/tickets'
+import { loadSelectedTickets, createTicket, loadTicket } from '../../actions/tickets'
 import TicketForm from '../tickets/TicketForm'
 
 class EventDetails extends PureComponent{    
@@ -11,6 +11,7 @@ class EventDetails extends PureComponent{
     this.props.loadEvent(this.props.match.params.id) 
     this.props.loadSelectedTickets(this.props.match.params.id)
     this.props.getUsers()
+
   }
 
   createTicket = (ticket) => {
@@ -18,9 +19,103 @@ class EventDetails extends PureComponent{
     this.props.createTicket(ticket)
   }
 
+  authorRisk = () => {
+    const authorId = this.props.tickets.map(tickets => {
+      return console.log(tickets)
+    })
+    const authorTickets = authorId.map(author => {
+      return (console.log(author))
+    }).length
+    console.log(authorTickets)
+    if (authorTickets === 1) {
+      return 10 
+    } else {
+      return 0
+    }
+  }
+  
+  // * if the ticket price is lower than the average ticket price for that event, that's a risk
+  priceRisk = () => {
+    const allTickets = this.props.tickets.length
+    const oneTicketPrice = this.props.ticket.price
+    const manyTicketsPrice = this.props.tickets.map(ticket => ticket.price)
+    const total = manyTicketsPrice.reduce((acc, value) => acc + value, 0)
+    const average = total / allTickets
+    // 	* if a ticket is X% cheaper than the average price, add X% to the risk 
+  // 	* if a ticket is X% more expensive than the average price, 
+  //deduct X% from the risk, with a maximum of 10% deduction
+    const risk = (100 * oneTicketPrice / average - 100)
+    if (risk < -10) {
+      return -10
+    } else {
+      return risk
+    }
+  }
+  
+  
+  // * if there are >3 comments on the ticket, add 5% to the risk
+  commentRisk = () => {
+    const allComments = this.props.comments.length
+    if (allComments > 3) {
+      return 5
+    } else {
+      return 0
+    }
+  }
+  
+  // if the ticket was added during business hours (9-17), 
+  //deduct 10% from the risk, if not, add 10% to the risk
+  dateRisk = () => {
+    const dateCreated = this.props.ticket.dateCreated
+    const hours = new Date(dateCreated)
+    const newHours = hours.getHours()
+    if (newHours >= 9 && newHours <= 17) {
+      return -10
+    } else {
+      return 10
+    }
+  }
+  
+  
+  // The minimal risk is 5% (there's no such thing as no risk) and the maximum risk is 95%.
+  totalRisk = () => {
+    const authorRisk = this.authorRisk()
+    const priceRisk = this.priceRisk()
+    const commentRisk = this.commentRisk()
+    const dateRisk = this.dateRisk()
+  
+    const risk = authorRisk + priceRisk + commentRisk + dateRisk
+  
+    if (risk < 5) {
+      return 5
+    } else if (risk > 95) {
+      return 95
+    } else {
+      return Math.round(risk)
+    }
+  }
+  
+  //End of code for risk
+
+
+  //To show color for risk on this page as well
+  //Map over ticket, get data I need
+  //Then calculate risk again
   render() {
     const {tickets} = this.props
     const {event} = this.props
+ 
+
+   const riskAgain = () => {
+    const ticketsObject = this.props.tickets.map(ticket => {
+      return ticket
+    })
+    console.log(ticketsObject)
+    }
+
+    riskAgain()
+    const {ticket} = this.props
+    console.log(this.props)
     if (event) {
       return (
         <div>
@@ -40,6 +135,7 @@ class EventDetails extends PureComponent{
                 <li>{ticket.price}</li>
                 <li>{ticket.description}</li>
                 <li>{ticket.dateCreated}</li>
+                <li>Risk: {this.totalRisk()}</li>
                 <li><Link to={`/tickets/${ticket.id}`}>Ticket details</Link></li>
               </ul>))}
               {this.props.currentUser && <div>
@@ -59,8 +155,9 @@ const mapStateToProps = function (state) {
     event: state.event,
     tickets: state.tickets,
     currentUser: state.currentUser,
-    users: state.users
+    users: state.users,
+    ticket: state.ticket
   }
 }
 
-export default connect(mapStateToProps, { loadEvent , loadSelectedTickets, createTicket, getUsers })(EventDetails)
+export default connect(mapStateToProps, { loadEvent , loadSelectedTickets, createTicket, getUsers, loadTicket })(EventDetails)
