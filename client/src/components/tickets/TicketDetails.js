7d195d58aabfa5feb5c7e283af9a151b3c8d6ac7
DASHA <dariaepaneshnikova@icloud.com>
2019-04-03 13:53:16 +0200

    tested risk algorithm on the frontend by adding more tickets with different prices and timestamps, seems to be working. Added mathround to round the totalrisk displayed

--- client/src/components/tickets/TicketDetails.js
+++ client/src/components/tickets/TicketDetails.js
@@ -112,16 +112,11 @@ totalRisk = () => {
   } else if (risk > 95) {
     return 95
   } else {
-    return risk
+    return Math.round(risk)
   }
 }
 
-
-
-
-
-
-  //End of code for risk
+//End of code for risk
 
 
   render() {
@@ -144,10 +139,10 @@ totalRisk = () => {
               <p>Seller email: {ticket.user.firstName}</p>
               <p>URL: {ticket.url}</p>
               <p>Description: {ticket.description}</p>
-              <p>Id: {ticket.id}</p>
+              <p>Ticket Id: {ticket.id}</p>
               <p>Price: {ticket.price}</p>
-              <p>Date: {ticket.dateCreated}</p>
-              <p>Risk of this ticket being fraudulent: {this.totalRisk()} % </p>
+              <p>Date Created: {ticket.dateCreated}</p>
+              <p>Risk of this ticket being fraudulent: {this.totalRisk()}% </p>
             </div>
             }  
         
@@ -169,9 +164,6 @@ totalRisk = () => {
            <CommentsForm onSubmit={this.createComment} />
          </div>
         }
-
-
-        <CommentsForm onSubmit={this.createComment} />
         <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
 
 
@@ -195,26 +187,3 @@ const mapStateToProps = function (state) {
 }
 
 export default connect(mapStateToProps, { loadTicket, editTicket, loadTickets, getUsers, createComment, loadSelectedComments, loadComments })(TicketDetails)
-
-// ## !! Fraud risk algorithm !!
-
-// _This is an important part of the assignment. If you only finish one thing, it should be this thing!_
-
-// Tickets can be fraudulent, and as a customer I don't want to buy a fake ticket! So, we want to show customers the risk that they are taking when buying the ticket. 
-
-// On the ticket page for a specific ticket, we want to show a text like:
-
-// > "We calculated that the risk of this ticket being a fraud is XX%"
-
-// The percentage should be calculated using the following algorithm:
-
-// * if the ticket is the only ticket of the author, add 10%
-// * if the ticket price is lower than the average ticket price for that event, that's a risk
-// 	* if a ticket is X% cheaper than the average price, add X% to the risk 
-// 	* if a ticket is X% more expensive than the average price, deduct X% from the risk, with a maximum of 10% deduction
-// * if the ticket was added during business hours (9-17), deduct 10% from the risk, if not, add 10% to the risk
-// * if there are >3 comments on the ticket, add 5% to the risk
-
-// The minimal risk is 5% (there's no such thing as no risk) and the maximum risk is 95%.
-
-// The calculated risk of a ticket depends on many factors. Make sure that the risk value is always "live" (i.e. up to date)
